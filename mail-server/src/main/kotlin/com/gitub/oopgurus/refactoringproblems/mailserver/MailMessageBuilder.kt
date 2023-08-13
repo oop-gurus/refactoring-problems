881025f8c4f5f9d6fbb5ec1685a59d1f81dda825
lette1394 <lette1394@gmail.com>
2023-08-14 00:10:44 +0900

    refactor: 세련된 when 구문으로 변경

--- mail-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/mailserver/MailMessageBuilder.kt
+++ mail-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/mailserver/MailMessageBuilder.kt
@@ -196,11 +196,9 @@ class MailMessageBuilder(
     }
 
     private fun toScheduled(mailMessage: MailMessage): MailMessage {
-        val sendAfter = getSendAfter()
-        val scheduled = if (sendAfter == null) {
-            mailMessage
-        } else {
-            ScheduledMailMessage(
+        val scheduled = when (val sendAfter = getSendAfter()) {
+            null -> mailMessage
+            else -> ScheduledMailMessage(
                 mailMessage = mailMessage,
                 scheduledExecutorService = scheduledExecutorService,
                 sendAfter = sendAfter,
