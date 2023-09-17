f2c39a6b9c83ff2954ea1d2b6b48e855798dc1c5
lette1394 <lette1394@gmail.com>
2023-09-17 16:50:50 +0900

    내부 private 구현을 숨기는 구현의 문제점

--- config-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/configserver/Properties.kt
+++ config-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/configserver/Properties.kt
@@ -16,6 +16,7 @@ class Properties(
     // ConfigSearchService(외부): Properties(내부)의 필드에 접근이 필요함
     // Properties(내부): 내 정보를 외부에 보여줘야 하는건 알겠는데, 어떤 정보가 필요할지 모르니 다 열어줘야함
     // 아이디어 -> Properties(내부)가 어떤 정보를 주는지 결정하면 안될까?
+    // 문제점: 표현의 가짓수가 많아질수록 return 값으로 표현하는게 너무 힘들다... 어떡하지?
     fun okay_i_will_give_you_what_you_want(whatIWant: What_I_Want): Any {
         return when (whatIWant) {
             What_I_Want.DESCRIPTIONS -> descriptions()
