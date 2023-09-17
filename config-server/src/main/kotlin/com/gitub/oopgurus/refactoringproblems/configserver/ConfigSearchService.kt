88f04e4230de895fb95ac6f8327c4b0fe3993985
lette1394 <lette1394@gmail.com>
2023-09-17 17:49:07 +0900

    Person에도 동일한 방법 적용

--- config-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/configserver/ConfigSearchService.kt
+++ config-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/configserver/ConfigSearchService.kt
@@ -14,36 +14,9 @@ class ConfigSearchService(
 
     fun getConfig(id: Long): ConfigGetDto {
         val persons = personRepository.findAllByConfigId(id).map {
-            var isMobilePhone = false
-            var isOfficePhone = false
-            if (it.phone!!.startsWith("010")) {
-                isMobilePhone = true
-            }
-            if (it.phone!!.startsWith("02")) {
-                isOfficePhone = true
-            }
-
-            // check firstname has alphabet
-            val isKorean = it.firstName!!.matches(Regex("[가-힣]+"))
-
-            // 이건 매번 꺼내서 작업하기 너무 귀찮은데...
-            // 그냥 whatIWant.result() -> 이러면 PersonDto가 나오면 안되나?
-            PersonDto(
-                id = it.id!!,
-                name = if (isKorean) {
-                    "${it.firstName}${it.lastName}"
-                } else {
-                    "${it.lastName}${it.firstName}"
-                },
-                email = it.email,
-                phone = it.phone,
-                isForeigner = !isKorean,
-                isKorean = isKorean,
-                firstName = it.firstName!!,
-                lastName = it.lastName!!,
-                isMobilePhone = isMobilePhone,
-                isOfficePhone = isOfficePhone,
-            )
+            val builder = PersonDtoBuilder()
+            Person(it).okay_i_will_give_you_what_you_want(builder)
+            builder.result()
         }
         val system = systemRepository.findByConfigId(id)?.let {
             SystemDto(
--- config-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/configserver/Person.kt
+++ config-server/src/main/kotlin/com/gitub/oopgurus/refactoringproblems/configserver/Person.kt
@@ -4,13 +4,28 @@ class Person(
     private val entity: PersonEntity,
 ) {
 
+    fun okay_i_will_give_you_what_you_want(personDtoBuilder: PersonDtoBuilder) {
+        personDtoBuilder.name(name())
+        personDtoBuilder.firstName(firstName())
+        personDtoBuilder.lastName(lastName())
+        personDtoBuilder.email(email())
+        personDtoBuilder.phone(phone())
+        personDtoBuilder.isKorean(isKorean())
+        personDtoBuilder.isMobilePhone(isMobilePhone())
+        personDtoBuilder.isOfficePhone(isOfficePhone())
+    }
+
     private fun name(): String {
         return if (isKorean()) {
-            "${entity.firstName}${entity.lastName}"
+            "${firstName()}${lastName()}"
         } else {
-            "${entity.lastName}${entity.firstName}"
+            "${lastName()}${firstName()}"
         }
     }
+
+    private fun lastName() = entity.lastName!!
+
+    private fun firstName() = entity.firstName!!
 
     private fun email(): String {
         return entity.email!!
@@ -34,5 +49,66 @@ class Person(
 
     private fun isOfficePhone(): Boolean {
         return entity.phone!!.startsWith("02")
+    }
+}
+
+class PersonDtoBuilder {
+    private var idGet: () -> Long = { throw IllegalStateException() }
+    private var nameGet: () -> String = { throw IllegalStateException() }
+    private var firstNameGet: () -> String = { throw IllegalStateException() }
+    private var lastNameGet: () -> String = { throw IllegalStateException() }
+    private var emailGet: () -> String = { throw IllegalStateException() }
+    private var phoneGet: () -> String = { throw IllegalStateException() }
+    private var isKoreanGet: () -> Boolean = { throw IllegalStateException() }
+    private var isForeignerGet: () -> Boolean = { throw IllegalStateException() }
+    private var isMobilePhoneGet: () -> Boolean = { throw IllegalStateException() }
+    private var isOfficePhoneGet: () -> Boolean = { throw IllegalStateException() }
+
+    fun name(name: String) {
+        nameGet = { name }
+    }
+
+    fun firstName(firstName: String) {
+        firstNameGet = { firstName }
+    }
+
+    fun lastName(lastName: String) {
+        lastNameGet = { lastName }
+    }
+
+    fun email(email: String) {
+        emailGet = { email }
+    }
+
+    fun phone(phone: String) {
+        phoneGet = { phone }
+    }
+
+    fun isKorean(isKorean: Boolean) {
+        isKoreanGet = { isKorean }
+        isForeignerGet = { !isKorean }
+    }
+
+    fun isMobilePhone(isMobilePhone: Boolean) {
+        isMobilePhoneGet = { isMobilePhone }
+    }
+
+    fun isOfficePhone(isOfficePhone: Boolean) {
+        isOfficePhoneGet = { isOfficePhone }
+    }
+
+    fun result(): PersonDto {
+        return PersonDto(
+            id = idGet(),
+            name = nameGet(),
+            firstName = firstNameGet(),
+            lastName = lastNameGet(),
+            email = emailGet(),
+            phone = phoneGet(),
+            isKorean = isKoreanGet(),
+            isForeigner = isForeignerGet(),
+            isMobilePhone = isMobilePhoneGet(),
+            isOfficePhone = isOfficePhoneGet(),
+        )
     }
 }
